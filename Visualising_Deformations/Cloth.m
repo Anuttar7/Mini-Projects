19bdf3350259f2cd8cb91c6c93f3855981a53995
Anuttar <anuttarr12@gmail.com>
2026-09-11 15:09:37 +0530

    Removed splitwise project and renamed existed directories

--- Visualising_Deformations/Cloth.m
+++
@@ -1,5 +0,0 @@
-n = 101;
-[x,y] = meshgrid(linspace(-10,10,n), linspace(-10,10,n));
-z = Cloth_F(x,y);
-C = abs(z);
-surf(x,y,z);
--- Visualising_Deformations/Cloth_F.m
+++
@@ -1,5 +0,0 @@
-function [z] = Cloth_F(x,y)
-    k = 0.01;
-    r = sqrt(x.^2 + y.^2);
-    z = k*exp(-0.1*r).*cos(r);
-end
